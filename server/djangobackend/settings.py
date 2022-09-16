25e323288cb2d23633d3599e156b5850e7b801df
Ozi <oguzkaanyildiz@mac-ozi.local>
2022-09-16 16:29:58 +0200

    Check the workflow

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -25,9 +25,7 @@ SECRET_KEY = 'ao5z(o(z@cvzodm99d32jkxa5e8a1!q_4sqss5-a%n6tg$#h$+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-APPEND_SLASH = True
-
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["dealership-reviews-ozi.eu-gb.cf.appdomain.cloud"]
 
 
 # Application definition
